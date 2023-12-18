b25cef08fefbcd5abdbdb31f098e5b2bdc7cb145
shubhh123 <shubhh1181@gmail.com>
2023-12-18 22:47:52 +0530

    Connected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
+
 import express from "express"
+import mongoose from "mongoose";
+
+
+mongoose.connect("mongodb://localhost:27017/estate")
+    .then(() => {
+        console.log("Connected to Database");
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
 
 const app = express();
 const port = 3000;
