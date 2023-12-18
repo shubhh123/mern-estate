34df14ba117092179c821631006597272f2b6287
shubhh123 <shubhh1181@gmail.com>
2023-12-18 15:49:26 +0530

    Created Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,20 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 
+/*
+In a single-page React Application, 
+routing refers to the process of navigating 
+between different pages without triggering a 
+full page reload. The application initially 
+loads a single HTML page. Then, it dynamically 
+renders different components based on user interaction.
+*/
 export default function App() {
   return <BrowserRouter>
+  <Header/>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<SignIn />} />
