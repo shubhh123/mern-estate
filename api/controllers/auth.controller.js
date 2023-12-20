5c02780f69913d642dc03e699c92477637748358
shubhh123 <shubhh1181@gmail.com>
2023-12-20 15:54:29 +0530

    completed signin page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -29,14 +29,23 @@ export const signin = async(req, res, next)=>{
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if (!validPassword) return next(errorHandler(404, "Wrong credentials"));
 
+
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET); 
 
         //To omit user password being sent over
-        const{password, ...restOfTheUserInfo} = validUser._doc;
+        /*
+            password: pass: It takes 
+            the password property from 
+            validUser._doc and assigns 
+            it to a variable named pass.
+        */
+
+        const{password: pass, ...restOfUserInfo} = validUser._doc;
+
         res
             .cookie('access_token', token, {httpOnly: true})
             .status(200)
-            .json(restOfTheUserInfo);
+            .json(restOfUserInfo);
 
     } catch (error) {
         next(error);
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignIn() {
+
+  const[formData, setFormData] = useState({});
+
+  const[error, setError] = useState(null);
+  const[loading, setloading] = useState(false);
+
+  const[loading2, setloading2] = useState(false);
+
+  const navigate = useNavigate();
+
+
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  console.log(formData);
+
+  const handleSubmit = async(e) => {
+      e.preventDefault();
+      
+      try {
+        
+        setloading(true);
+  
+        const res = await fetch("/api/user/auth/signin",
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData),
+          });
+          
+          const data = await res.json();
+  
+          if(data.success === false) {
+            setloading(false);
+            setError(data.message);
+            return;
+          }
+          setloading(false);
+          setError(null);
+
+          navigate('/');
+          console.log(data);
+        } catch(error) {
+          setloading(false);
+          setError(error.message);
+      }
+
+  }
+
+
   return (
-    <div>
-      Sign in
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        
+        <input type="email" onChange={handleChange} placeholder='email' className='border p-3 rounded-lg' id='email'/>
+        <input type="password" onChange={handleChange} placeholder='password' className='border p-3 rounded-lg' id='password'/>
+
+        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+        
+          {loading ? 'Loading...': 'Sign In'}
+        </button>
+
+      </form>
+
+      <div className='flex gap-2 mt-5'>
+        <p>Don't have an account?</p>
+
+        <Link to='/sign-up'>
+          <span className='text-blue-700 '>Sign Up</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
